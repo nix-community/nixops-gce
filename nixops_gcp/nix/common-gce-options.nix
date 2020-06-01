94563754e24cc165320a03114e9d4501f2bbbd64
Tewfik Ghariani <tewfik.ghariani@infor.com>
2020-06-01 02:07:11 +0100

    Example of Machines + GCE Disks. A somehow trivial example of frontend & backend machines alongside their respectively attached frontend-volume and backend-volume GCE disks + updating poetry.lock

--- nixops_gcp/nix/common-gce-options.nix
+++ nixops_gcp/nix/common-gce-options.nix
@@ -7,7 +7,7 @@ with lib;
   labels = mkOption {
       default = { };
       example = { foo = "bar"; xyzzy = "bla"; };
-      type = types.attrsOf types.str;
+      type = types.nullOr (types.attrsOf types.str);
       description = ''
         A set of key/value label pairs to assign to the instance.
       '';
