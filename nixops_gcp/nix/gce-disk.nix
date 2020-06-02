3b6f24aafadeda27d8b5bc1d18ab2401382ee70a
Tewfik Ghariani <tewfik.ghariani@infor.com>
2020-06-02 21:28:03 +0100

    Introducing imageOptions and defining it as submodule. The imageOptions contains the following :  - image : either image-resource or the name of an existing image  - family : the image family to be used  - project : the parent project containing the image/imageFamily Note that we have to specify either name or family. Default = null Then adjusted the image option to inherit from the imageOptions submodule in - gceDiskOptions in gce.nix - gce-disk.nix Finally, updated the bootstrapImage gce option accordingly

--- nixops_gcp/nix/gce-disk.nix
+++ nixops_gcp/nix/gce-disk.nix
@@ -45,21 +45,44 @@ with import <nixops/lib.nix> lib;
     };
 
     image = mkOption {
-      default = null;
-      example = "image-2cfda297";
-      type = types.nullOr ( types.either types.str (resource "gce-image") );
+      default  = null;
+      example = { name = null; family = "super-family"; project = "operations"; };
+      type = with types; (nullOr (submodule {
+        options = {
+          name = mkOption {
+            default = null;
+            example = "image-2cfda297";
+            type = types.nullOr (types.either types.str (resource "gce-image"));
+            description = ''
+              Name of an existent image or image-resource to be used.
+              Must specify the project if the image is defined as public.
+            '';
+          };
+          family = mkOption {
+            default = null;
+            example = "image-family-custom";
+            type = types.nullOr types.str;
+            description = ''
+              Image family to grab the latest non-deprecated image from.
+              Must specify the project if the image family is defined as public.
+            '';
+          };
+          project = mkOption {
+            default = null;
+            example = "gcp-project";
+            type = types.nullOr types.str;
+            description = ''
+              The parent project containing a GCE image that was made public
+              for all authenticated users.
+            '';
+          };
+        };
+      }));
       description = ''
-        The image name or resource from which this disk will be created. If
-        not specified, an empty disk is created.  Changing the
+        The image, image family or image-resource from which to create the GCE disk.
+        If not specified, an empty disk is created. Changing the
         image name has no effect if the disk already exists.
       '';
-    };
-
-    publicImageProject = mkOption {
-      default = null;
-      example = "nixos-gcp-project";
-      type = types.nullOr types.str;
-      description = "The parent project containing a GCE image that was made public for all authenticated users.";
     };
 
     diskType = mkOption {
@@ -74,12 +97,16 @@ with import <nixops/lib.nix> lib;
   };
 
   config = 
-    (mkAssert ( (config.snapshot == null) || (config.image == null) )
+    (mkAssert ( (config.snapshot == null) || (config.image.name == null)
+              || (config.image.family == null) )
               "Disk can not be created from both a snapshot and an image at once"
-      (mkAssert ( (config.size != null) || (config.snapshot != null) || (config.image != null) )
-                "Disk size is required unless it is created from an image or snapshot" {
-          _type = "gce-disk";
-        }
+      (mkAssert ( (config.image.name == null) || (config.image.family == null) )
+                  "Must specify either image name or image family"
+        (mkAssert ( (config.size != null) || (config.snapshot != null) || (config.image != null) )
+                    "Disk size is required unless it is created from an image or snapshot" {
+            _type = "gce-disk";
+          }
+        )
       )
     );
 
