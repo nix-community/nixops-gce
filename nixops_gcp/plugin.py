5b271b13891437c6a0d7256c0d53bbbebd588ed4
adisbladis <adisbladis@gmail.com>
2020-07-09 14:42:11 +0200

    Use the NixOps class interface from https://github.com/NixOS/nixops/pull/1383

--- nixops_gcp/plugin.py
+++ nixops_gcp/plugin.py
@@ -1,15 +1,21 @@
 import os.path
 import nixops.plugins
+from nixops.plugins import Plugin
+
+
+class NixopsGCPPlugin(Plugin):
+    @staticmethod
+    def nixexprs():
+        return [os.path.dirname(os.path.abspath(__file__)) + "/nix"]
+
+    @staticmethod
+    def load():
+        return [
+            "nixops_gcp.resources",
+            "nixops_gcp.backends.gce",
+        ]
 
 
 @nixops.plugins.hookimpl
-def nixexprs():
-    return [os.path.dirname(os.path.abspath(__file__)) + "/nix"]
-
-
-@nixops.plugins.hookimpl
-def load():
-    return [
-        "nixops_gcp.resources",
-        "nixops_gcp.backends.gce",
-    ]
+def plugin():
+    return NixopsGCPPlugin()
